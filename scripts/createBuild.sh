faec7daa1ead157ba8c125cd602b4a5aa12f2e85
adhi0331 <adhi03.ananthan@gmail.com>
2023-01-03 17:43:19 -0800

    update script to install yarn

--- scripts/createBuild.sh
+++ scripts/createBuild.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install 16.0.0
+. ~/.nvm/nvm.sh
+node -e "console.log('Running Node.js ' + process.version)"
+npm install --global yarn
+yarn --version
 cd /deploy
 sudo yarn build
+
